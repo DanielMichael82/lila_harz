4f5b9f221227628e06b121828743fb11416e4cf8
DanielMichael82 <danielmpearce@live.co.uk>
2021-06-29 15:53:20 +0000

    Created HTML template

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/collections")
+def collections():
+    return render_template("collections.html")
+
+
+@app.route("/care_guide")
+def care_guide():
+    return render_template("care_guide.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
