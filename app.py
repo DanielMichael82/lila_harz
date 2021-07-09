1ec9c2340151d81ed27dedf76d0eebd9ab5d9d17
DanielMichael82 <danielmpearce@live.co.uk>
2021-07-09 14:30:15 +0000

    Added json data for owner description

--- app.py
+++ app.py
@@ -1,47 +1,50 @@
 import os
+import json
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
 
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data = []
+    with open("data/lilaharz.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("index.html", lilaharz=data)
 
 
 @app.route("/collections")
 def collections():
-    return render_template("collections.html")
+    return render_template("collections.html", page_title="Collections")
 
 
 @app.route("/care_guide")
 def care_guide():
-    return render_template("care_guide.html")
+    return render_template("care_guide.html", page_title="Care Guide")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", page_title="Login")
 
 
 if __name__ == "__main__":
